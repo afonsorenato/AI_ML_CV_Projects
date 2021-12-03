25cedb65d8eca8682b94c135d0ce46e926694323
Renato Afonso <renato.miguel.afonso@gmail.com>
2021-12-03 20:57:23 +0000

    Organized better the code

--- AI Volume Control/main_VolumeControlUpgraded.py
+++ AI Volume Control/main_VolumeControlUpgraded.py
@@ -46,7 +46,6 @@ while True:
             volPer = smoothness * round(volPer/smoothness)
             volume.SetMasterVolumeLevelScalar(volPer//100, None)
 
-
             cv2.putText(img, f'{int(volPer)}', (40, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
             cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
             cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), -1)
