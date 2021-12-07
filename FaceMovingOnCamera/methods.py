300775ea0b11b10d6a1fc4b7d4680a6b8a405ac1
Renato Afonso <renato.miguel.afonso@gmail.com>
2021-12-07 13:44:28 +0000

    First commit with docker file
    Docker file for Face in System project

--- FaceMovingOnCamera/methods.py
+++ FaceMovingOnCamera/methods.py
@@ -1,10 +1,4 @@
 import cv2
-import numpy as np
-import time
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from itertools import product, combinations
-
 
 # returns the name of the file with chessboard
 def getName(i):
@@ -15,6 +9,7 @@ def getName(i):
     name = aux + aux2 + aux3
 
     return name
+
 
 # return the relation points real and in frame w/ chesssboard
 def getCalibPoints(objpoints, imgpoints, objp):
@@ -45,6 +40,7 @@ def getCalibPoints(objpoints, imgpoints, objp):
 
     return objpoints, imgpoints, objp, gray
 
+
 # print camera intrinsic parameters
 def printIntrinsics(mtx):
     fl1 = str(round(mtx[0][0]))
@@ -62,20 +58,20 @@ def printIntrinsics(mtx):
 
     return fl1
 
+
 # eye-box volume at a certain distance from camera
 def drawEyeBoxSize(faceDist, img):
-
-    #Assumed eye-box size at camera's position (0,0,0)
-    init_width = 160 #130
-    init_hight = 70  #60
+    # Assumed eye-box size at camera's position (0,0,0)
+    init_width = 160  # 130
+    init_hight = 70  # 60
 
     ratio_per_cm = 0.5
     w = int(init_width - ratio_per_cm * faceDist)
     h = int(init_hight - ratio_per_cm * faceDist)
 
     width, height = img.shape[:2]
-    x = int(width/2)
-    y = int(height/2-h)
+    x = int(width / 2)
+    y = int(height / 2 - h)
 
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
     aux = "Dist from camera: " + str(faceDist) + " cm"
@@ -84,9 +80,9 @@ def drawEyeBoxSize(faceDist, img):
 
     return x, y, w, h
 
+
 # Get calibration position in eye-box
 def printCalibPosition(centroid_eye_left, img, a, b, c, d, x, y, ex, ey, ew, eh):
-
     if ((a < (ex + x) and (x + ex + ew > a)) and (a + c > (ex + x) and (x + ex + ew < a + c))) and (
             (b < (ey + y) and (y + ey + eh > b)) and (b + d > (ey + y) and (y + ey + eh < b + d))):
         cv2.putText(img, 'Driver inside the Eye-box', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 20, 12), 1)
@@ -103,9 +99,9 @@ def printCalibPosition(centroid_eye_left, img, a, b, c, d, x, y, ex, ey, ew, eh)
 
     return
 
-def printCalibPositionDlib(img,centroid, x_left_eye, x_right_eye, a, b, c):
 
-    if (a < x_left_eye and x_right_eye < a+c):
+def printCalibPositionDlib(img, centroid, x_left_eye, x_right_eye, a, b, c):
+    if (a < x_left_eye and x_right_eye < a + c):
         cv2.putText(img, 'Driver inside the Eye-box', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 20, 12), 1)
 
         if (centroid > a) and (centroid < a + c / 3):
@@ -119,27 +115,16 @@ def printCalibPositionDlib(img,centroid, x_left_eye, x_right_eye, a, b, c):
 
     return
 
-#Estimate distance from face - camera
+
+# Estimate distance from face - camera
 def getFaceDist(w, face_ref):
     width_faceInPixels = w  # Width of my face (just testing)
     width_faceInCM = face_ref  # cm
     fl = 812 / 2
 
     dist = round(width_faceInCM * fl / width_faceInPixels)
-    #print("width pixels: " + str(round(width_faceInPixels)))
+    # print("width pixels: " + str(round(width_faceInPixels)))
     aux_str = "Dist from camera: " + str(dist) + "cm"
     print(aux_str)
 
     return dist
-
-
-################################################
-
-def getDrowsiness(landmarks, img):
-
-
-
-
-
-
-    return
