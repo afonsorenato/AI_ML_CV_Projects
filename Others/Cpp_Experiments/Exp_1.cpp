cc264106a3b9bf053c5b36fa2be50d9cdbbb5a01
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-20 15:28:59 +0100

    Created experiments in C++ (for recalling purposes)

---
+++ Others/Cpp_Experiments/Exp_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void introduction()
+{
+    cout << "\n************ Developed by Renato Afonso ************";
+    cout << "\n************      2022 - Portugal       ************ \n \n";
+}
+
+int main()
+{
+    introduction();
+
+    cout << "Test me\n";
+    return 0;
+}
