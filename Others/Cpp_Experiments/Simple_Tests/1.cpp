51e92ef6d8ed33b86da6a55261df56a424721edc
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-21 16:10:52 +0100

    First example

---
+++ Others/Cpp_Experiments/Simple_Tests/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello world!" << std::endl;
+
+    //Run time error
+    int value = 7/0;
+    std::cout << "value: " << value << std::endl;
+
+
+    return 0;
+}
