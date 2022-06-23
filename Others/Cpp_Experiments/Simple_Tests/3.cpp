5dc8f69e180becde2beaf3f3e3a6fd84147d25fe
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-23 11:07:30 +0100

    String test

--- Others/Cpp_Experiments/Simple_Tests/3.cpp
+++ Others/Cpp_Experiments/Simple_Tests/3.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <string>
 
 int main(){
 
     int age;
+    int number;
     std::string full_name;
 
-    std::cout << "Please type your full name: " << std::endl;
+    std::cout << "Please type your full name:  " << std::endl; 
     std::getline(std::cin, full_name);
 
     std::cout << "Type your age: " << std::endl;
     std::cin >> age;
     std::cout << "Hello " << full_name << "! You are " << age << " years old." << std::endl;
 
+    std::cerr << "Error message: wrong!" << std::endl;
+    std::clog << "Log message \n" << std::endl;
+
+    return 0;
 }
