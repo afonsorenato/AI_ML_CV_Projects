b72751655e522253b23c31b2344aada64f7759f8
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-23 18:21:15 +0100

    Cleaned the dir

--- 3D Experiments/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Renato/OneDrive/Documentos/GitHub/CV_Projects/3D Experiments
-# Build directory: C:/Users/Renato/OneDrive/Documentos/GitHub/CV_Projects/3D Experiments/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ 3D Experiments/test1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(){
+
+    system("CLS");
+
+    std::cout << "Hello!\n" <<std::endl;
+
+    Mat image;
+
+    return 0;
+}
