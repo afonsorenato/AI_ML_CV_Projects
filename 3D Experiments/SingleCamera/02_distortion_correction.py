9abf6632f9b5494452ed02e8a193bd24bf590297
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-26 21:25:12 +0100

    Concluded distortion correction script

--- 3D Experiments/SingleCamera/02_distortion_correction.py
+++ 3D Experiments/SingleCamera/02_distortion_correction.py
@@ -7,20 +7,32 @@ from yaml.loader import SafeLoader
 image_path = "Chessboard_Samples/calib (22).jpg"
 k_matrix_path = 'calibration_matrix.yaml'
 
-
 # Method to correct distortion of a given image, knowing the camera K matrix
 def undistortImage(mtx, dist, img):
     h, w = img.shape[:2]
+    print(h, w)
 
     # Refining the camera matrix using parameters obtained by calibration
+    alpha = 1
+    # alpha = 0 --> returns undistorted image with min unwanted pixels
+    # alpha = 1 --> pixels are retained with some extra black images
     newcamera_mtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 
     # Method 1 to Undistorted the image
     dst = cv2.undistort(img, mtx, dist, None, newcamera_mtx)
 
+    x, y, w, h = roi
+    dst = dst[y:y + h, x:x + w]
+    cv2.imwrite('Results/calibResult_method1.png', dst)
+
     # Method 2 to undistorted the image
     mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcamera_mtx, (w, h), 5)
     dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+
+    # crop the image
+    x, y, w, h = roi
+    dst_2 = dst[y:y + h, x:x + w]
+    cv2.imwrite('Results/calibResult_method2.png', dst_2)
 
     # Displaying the undistorted image
     cv2.imshow("Undistorted Image", dst)
--- 3D Experiments/SingleCamera/cam_config.py
+++ 3D Experiments/SingleCamera/cam_config.py
@@ -15,3 +15,24 @@ CHECKERBOARD = (8, 5)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
             30,  # iterations
             0.001)  # epsilon
+
+
+def estimateReporjectionERror(objpoints, imgpoints, rvecs, tvecs, mtx, dist):
+    """Re-projection error gives a good estimation of just how exact is the
+    found parameters. This should be as close to zero as possible. Given the
+    intrinsic, distortion, rotation and translation matrices, we first
+    transform the object point to image point using cv2.projectPoints().
+    Then we calculate the absolute norm between what we got with our
+    transformation and the corner finding algorithm.
+        """
+    mean_error = 0
+    tot_error = 0
+
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+        tot_error += error
+
+    print("Total error: ", mean_error / len(objpoints))
+
+
