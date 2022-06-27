cc094b276fd6d53d6aaf299b419f456172142c76
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-27 14:00:40 +0100

    Depth map working fine at <1FPS

--- 3D Experiments/SingleCamera/04_monocular_Depth_Est.py
+++ 3D Experiments/SingleCamera/04_monocular_Depth_Est.py
@@ -8,7 +8,7 @@ path_model = "Models/"
 
 # Read network
 model_name = "model-f6b98070.onnx"  # large model
-# model_name = "model-small.onnx"  # small model
+#model_name = "model-small.onnx"  # small model
 
 # Load the Dnn model
 model = cv2.dnn.readNet(path_model + model_name)
@@ -16,33 +16,39 @@ model = cv2.dnn.readNet(path_model + model_name)
 if model.empty():
     print("Could not load|!\n")
 
-
 # Webcam
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
 
     ok, img = cap.read()
-
     imgH, imgW, chan = img.shape
 
     # Fps
     start = time.time()
 
+    # Create blob from input image
+    blob = cv2.dnn.blobFromImage(img, 1/255., (384, 384), (123.675, 116.28, 103.53), True, False)
 
+    # Set input to the model
+    model.setInput(blob)
 
+    # Make forward pass in model
+    output = model.forward()
+    output = output[0, :, :]
+    output = cv2.resize(output, (imgW, imgH))
+
+    # Normalize the output
+    output = cv2.normalize(output, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
     # End time
     end = time.time()
-    fps = 1/(end-start)
-    cv2.putText(img, f"{fps:.2f} FPS", (50, 50),
-                FONT, 4, RED, 2)
-
+    fps = 1 / (end - start)
+    cv2.putText(img, f"{fps:.2f} FPS",
+                (50, 50), FONT, 1, RED, lINE_THICKNESS)
 
     cv2.imshow("Image", img)
-    #cv2.imshow("Depth Map", output)
+    cv2.imshow("Depth Map", output)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
-
-
