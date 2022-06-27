5993542f9cc962cdc6f1d6a012ffffbeaf0d7464
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-27 14:11:45 +0100

    Saves png of depth image in gray scale

--- 3D Experiments/SingleCamera/04_monocular_Depth_Est.py
+++ 3D Experiments/SingleCamera/04_monocular_Depth_Est.py
@@ -1,14 +1,17 @@
 import cv2
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 from cam_config import *
+from mpl_toolkits.mplot3d import Axes3D
 
+# Path where the model is
 path_model = "Models/"
 
 # Read network
 model_name = "model-f6b98070.onnx"  # large model
-#model_name = "model-small.onnx"  # small model
+# model_name = "model-small.onnx"  # small model
 
 # Load the Dnn model
 model = cv2.dnn.readNet(path_model + model_name)
@@ -28,7 +31,7 @@ while cap.isOpened():
     start = time.time()
 
     # Create blob from input image
-    blob = cv2.dnn.blobFromImage(img, 1/255., (384, 384), (123.675, 116.28, 103.53), True, False)
+    blob = cv2.dnn.blobFromImage(img, 1 / 255., (384, 384), (123.675, 116.28, 103.53), True, False)
 
     # Set input to the model
     model.setInput(blob)
@@ -44,11 +47,12 @@ while cap.isOpened():
     # End time
     end = time.time()
     fps = 1 / (end - start)
-    cv2.putText(img, f"{fps:.2f} FPS",
-                (50, 50), FONT, 1, RED, lINE_THICKNESS)
+    cv2.putText(img, f"{fps:.2f} FPS", (50, 50), FONT, 1, RED, lINE_THICKNESS)
 
     cv2.imshow("Image", img)
     cv2.imshow("Depth Map", output)
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite('./Results/depth_map1.png', output*255)
         break
+
