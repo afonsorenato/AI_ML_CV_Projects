72ed98243ac921a438796a44adb1bc41089f2205
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-27 12:25:40 +0100

    Create 04_monocular_Depth_Est.py

---
+++ 3D Experiments/SingleCamera/04_monocular_Depth_Est.py
@@ -0,0 +1,48 @@
+import cv2
+import time
+import numpy as np
+
+from cam_config import *
+
+path_model = "models/"
+
+# Read network
+model_name = "model-f6b98070.onnx"  # large model
+# model_name = "model-small.onnx"  # small model
+
+# Load the Dnn model
+model = cv2.dnn.readNet(path_model + model_name)
+
+if model.empty():
+    print("Could not load|!\n")
+
+
+# Webcam
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+
+    ok, img = cap.read()
+
+    imgH, imgW, chan = img.shape
+
+    # Fps
+    start = time.time()
+
+
+
+
+    # End time
+    end = time.time()
+    fps = 1/(end-start)
+    cv2.putText(img, f"{fps:.2f} FPS", (50, 50),
+                FONT, 4, RED, 2)
+
+
+    cv2.imshow("Image", img)
+    #cv2.imshow("Depth Map", output)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+
