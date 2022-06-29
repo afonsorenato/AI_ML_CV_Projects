74f55d2535a973901b88378029a57ec316d1f99a
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-29 11:22:00 +0100

    Simple PC visualization with open3D

---
+++ 3D Experiments/PointCloudsExperiments/01_pointOpen3D.py
@@ -0,0 +1,20 @@
+
+"""
+*********** Renato Afonso - 2022 ***********
+Some revisions / recalling some 3D vision concepts
+"""
+
+import open3d as o3d
+import numpy as np
+import sys
+import os
+
+
+# Reads the ply point-cloud file
+# This one as 173000 points to draw the 3D object
+#pcd = o3d.io.read_point_cloud("Samples_PointClouds/armadillo.ply")
+pcd = o3d.io.read_point_cloud("Samples_PointClouds/fragment.ply")
+
+print(pcd)
+print(np.asarray(pcd.points))
+o3d.visualization.draw_geometries([pcd])
---
+++ 3D Experiments/PointCloudsExperiments/notes.md
@@ -0,0 +1,9 @@
+
+*********** Renato Afonso - 2022 ***********
+Some revisions / recalling some 3D vision concepts
+
+
+
+### Some notes:
+Modern library for 3D data processing
+
