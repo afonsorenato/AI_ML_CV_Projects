fc6ec3cea9036e00f5e296eb7045027b06ac6e13
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-30 14:05:27 +0100

    Update 06_smartphonePointCloud.py

--- 3D Experiments/PointCloudsExperiments/06_smartphonePointCloud.py
+++ 3D Experiments/PointCloudsExperiments/06_smartphonePointCloud.py
@@ -1,6 +1,7 @@
 import cv2
 import yaml
 import torch
+import imutils
 import numpy as np
 import open3d as o3d
 import matplotlib.pyplot as plt
@@ -12,3 +13,17 @@ cloud to the target point cloud.
 
 The output is a refined transformation that alights the two point clouds.
 """
+
+images = [cv2.imread("Samples_PointClouds/Room_Reconstruct/00001.tiff"),
+          cv2.imread("Samples_PointClouds/Room_Reconstruct/00020.tiff")]
+
+
+stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
+(status, stitched) = stitcher.stitch(images)
+
+print(status)
+print(stitched)
+
+cv2.namedWindow("Results", cv2.WINDOW_NORMAL)
+cv2.imshow("Results", stitched)
+cv2.waitKey(0)
