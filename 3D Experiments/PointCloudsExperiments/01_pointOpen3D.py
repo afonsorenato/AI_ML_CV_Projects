ae31463c7f7c258372999842d5548f74cdee0aa4
Renato Afonso <renato.miguel.afonso@gmail.com>
2022-06-30 15:21:47 +0100

    Update 01_pointOpen3D.py

--- 3D Experiments/PointCloudsExperiments/01_pointOpen3D.py
+++ 3D Experiments/PointCloudsExperiments/01_pointOpen3D.py
@@ -13,7 +13,9 @@ import os
 # Reads the ply point-cloud file
 # This one as 173000 points to draw the 3D object
 #pcd = o3d.io.read_point_cloud("Samples_PointClouds/armadillo.ply")
-pcd = o3d.io.read_point_cloud("Samples_PointClouds/fragment.ply")
+#pcd = o3d.io.read_point_cloud("Samples_PointClouds/fragment.ply")
+pcd = o3d.io.read_point_cloud("Samples_PointClouds/my_room2.ply")
+
 
 print(pcd)
 print(np.asarray(pcd.points))
